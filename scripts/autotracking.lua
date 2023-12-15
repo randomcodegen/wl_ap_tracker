103cb7d6f8b9c69261e133264977ec36d166097e
randomcodegen <pathofstreaming@gmail.com>
2023-12-15 08:29:56 +0100

    Fixed syntax for Parsley Woods 31 and autotracking

--- scripts/autotracking.lua
+++ scripts/autotracking.lua
@@ -134,15 +134,16 @@ function onLocation(location_id, location_name)
         obj.AvailableChestCount = obj.AvailableChestCount - 1
     end
 	--handle specific cases for logic
-	if loc_name=="Rice Beach 05" and loc_type=="Boss" then
+	if loc_name=="Rice Beach 05 - Boss" and loc_type=="Boss" then
 		local obj = Tracker:FindObjectForCode("rb_flooded")
 		obj.Active = true
 	end
-	if loc_name=="Parsley Woods 32" and loc_type=="Normal Exit" then
+	if loc_name=="Parsley Woods 32 - Normal Exit" and loc_type=="Normal Exit" then
 		local obj = Tracker:FindObjectForCode("pw_drained")
 		obj.Active = true
 	end
-	if loc_name=="Mt. Teapot 12" and loc_type=="Normal Exit" then
+	if loc_name=="Mt. Teapot 12 - Normal Exit" and loc_type=="Normal Exit" then
+		print("Unlock mtt13")
 		local obj = Tracker:FindObjectForCode("mt_boss_tile_unlocked")
 		obj.Active = true
 	end
